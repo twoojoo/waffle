c4d6f96b5192d8c4d4d8982cb0e4d66770ffdf25
Giovanni Campeol <giovanni@smartpricing.it>
2023-01-22 09:11:12 +0100

    enhanced rest example

--- examples/rest.ts
+++ examples/rest.ts
@@ -2,7 +2,12 @@ import { Server } from "../src"
 
 const server = Server({ logger: true })
 
-let users = [
+type User = {
+	id: number,
+	name: string,
+}
+
+let users: User[] = [
 	{ id: 1, name: "John" }, 
 	{ id: 2, name: "Angela" }, 
 	{ id: 3, name: "Bob" }
@@ -12,19 +17,23 @@ let users = [
 
 server.version(1)
 	.prefix("users")
-	.get(":id").handler(async (req, rep) => rep.send(users.find(u => u.id == req.body.id)))
-	.delete(":id").handler(async (req, rep) => {
+
+	.get<{ Params: { id: number } }>(":id").handler(async (req, rep) => rep.send(users.find(u => u.id == req.params.id)))
+
+	.delete<{ Params: { id: number } }>(":id").handler(async (req, rep) => {
 		const id = (req.params as any).id
 		const user = users.find(u => u == id)
 		users = users.filter(u => u != id)
 		rep.send(user)
 	})
-	.post().handler(async (req, rep) => {
-		users.push(req.body as any)
-		rep.send(req.body as any)
+
+	.post<{ Body: User }>().handler(async (req, rep) => {
+		users.push(req.body)
+		rep.send(req.body)
 	})
-	.put(":id").handler(async (req, rep) => {
-		const index = users.findIndex(u => u.id == (req.params as any).id)
-		users[index].name = (req.body as any).name
+
+	.put<{ Params: { id: number }, Body: Omit<User, "id"> }>(":id").handler(async (req, rep) => {
+		const index = users.findIndex(u => u.id == req.params.id)
+		users[index].name = req.body.name
 		rep.send(users[index])
 	})
