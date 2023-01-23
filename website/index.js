ea06c35bd414f5c0bdc16b8897e8734a3a638dcc
twoojoo <twoojoo95@gmail.com>
2023-01-23 10:07:22 +0100

    added external links

--- website/index.js
+++ website/index.js
@@ -1,7 +1,7 @@
-import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/highlight.min.js';
-// import ts from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/languages/typescript.min.js';
-// hljs.registerLanguage("typescript", ts)
-hljs.highlightAll()
+// import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/highlight.min.js';
+// // import ts from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/languages/typescript.min.js';
+// // hljs.registerLanguage("typescript", ts)
+// hljs.highlightAll()
 
 const codeBlocks = document.getElementsByClassName("code-block")
 
@@ -12,3 +12,11 @@ for (const cb of codeBlocks) {
 		navigator.clipboard.writeText(cb.textContent.split("⎘")[1].split("\n")[1].trim());
 	})
 }
+
+const extLink = document.getElementById("external-links")
+
+console.log(extLink)
+
+window.addEventListener("scroll", () => {
+	extLink.style.top = `${window.scrollY}px`
+})
