1d120f6d704bdb15d8e56bcacfb9cb918a6c98e8
twoojoo <twoojoo95@gmail.com>
2023-01-23 10:56:16 +0100

    added menu in website

--- docs/index.js
+++ docs/index.js
@@ -7,7 +7,6 @@ const codeBlocks = document.getElementsByClassName("code-block")
 
 for (const cb of codeBlocks) {
 	const copyItem = cb.firstElementChild
-	console.log(copyItem)
 	copyItem.addEventListener("click", item => {
 		navigator.clipboard.writeText(cb.textContent.split("⎘")[1].split("\n")[1].trim());
 	})
@@ -20,3 +19,18 @@ console.log(extLink)
 window.addEventListener("scroll", () => {
 	extLink.style.top = `${window.scrollY}px`
 })
+
+const menuItems = document.getElementsByClassName("main-menu-item")
+for (const menuItem of menuItems) {
+	const codeSample = document.getElementById(menuItem.id.split("-")[1])
+	const scrollOffset = window.innerHeight * 0.10
+	menuItem.addEventListener("click", () => window.scrollTo(0, getOffset(codeSample).top - scrollOffset))
+}
+
+function getOffset(el) {
+	const rect = el.getBoundingClientRect();
+	return {
+		left: rect.left + window.scrollX,
+		top: rect.top + window.scrollY
+	};
+}
