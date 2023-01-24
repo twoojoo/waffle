b8523e6d02e623cd4cb9685af4901e1786b130dd
twoojoo <twoojoo95@gmail.com>
2023-01-24 23:17:13 +0100

    route specific limiter working

--- examples/ping.ts
+++ examples/ping.ts
@@ -1,4 +1,8 @@
 import { Waffle } from "../src"
 
 //default address: 0.0.0.0:80
-Waffle().GET("/ping").handler(async (_, rep) => rep.send("pong")).listen()
+Waffle()
+	.address("localhost", 3000)
+	.GET("/ping")
+	.handler(async (_, rep) => rep.send("pong"))
+	.listen()
--- src/components/listen.ts
+++ src/components/listen.ts
@@ -10,15 +10,26 @@ export function listenFactory(serverCtx: ServerContext): Listen {
   return {
     listen(opts: Omit<FastifyListenOptions, "port" | "host"> = {}, callback?: (err: any, addr: string) => any) {
       const { host, port } = serverCtx
-      
-      serverCtx.fastify.register(rateLimiter, serverCtx.limiterOptions).then(() => {
 
-        serverCtx.routes.forEach(route => serverCtx.fastify.route(route))
+      const rateLimiterRegistered = !!serverCtx.limiterOptions //
+      const routesUseRateLimter = !!serverCtx.routes.find(r => (r.config as any).rateLimit)
 
+      if (!rateLimiterRegistered && routesUseRateLimter) serverCtx.limiterOptions = {}
+
+      if (serverCtx.limiterOptions) {
+        console.log("with rate limiter")
+        serverCtx.fastify.register(rateLimiter, serverCtx.limiterOptions).then(() => {
+          serverCtx.routes.forEach(route => serverCtx.fastify.route(route))
+          serverCtx.fastify.listen({ ...opts, host, port })
+            .then(_ => callback && callback(null, host + ":" + port))
+            .catch(err => callback && callback(err, host + ":" + port))
+        })
+      } else {
+        console.log("without rate limiter")
         serverCtx.fastify.listen({ ...opts, host, port })
           .then(_ => callback && callback(null, host + ":" + port))
           .catch(err => callback && callback(err, host + ":" + port))
-      })
+      }
     }
   }
 }
