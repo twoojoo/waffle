218da02cc7591ec5c20ff263ef73e673a1ba3900
twoojoo <twoojoo95@gmail.com>
2023-01-30 20:38:24 +0100

    added response and reply types to export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { HookWithPayloadCallback, HookCallback, OnErrorHookCallback } from "./components/hooks"
+export { FastifyServerOptions, FastifyRequest, FastifyReply } from 'fastify'
 export { RateLimitOptions } from "./components/limiter"
 export { DefaultRouteTypes } from "./components/routes"
-export { FastifyServerOptions } from 'fastify'
 export { Waffle } from "./components/server"
